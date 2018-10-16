c3f4dc5da92f8e6bca5e5eb4a4bc3e075b79b6e4
sk <xyk_sk@163.com>
2018-10-16 11:33:15 +0800

    1

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,4 +3,4 @@ from flask import Blueprint
 main = Blueprint('main',__name__)
 
 from .import errors
-from app.views import index,repository,products
+from app.views import index,repository,products,workplace
