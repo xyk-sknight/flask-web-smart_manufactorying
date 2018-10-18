886404eab51a140fcbc7b340d31bb32bf141fbc8
sk <xyk_sk@163.com>
2018-10-18 21:06:41 +0800

    工作台页面增加设备列表接口

--- app/views/workplace.py
+++ app/views/workplace.py
@@ -1,9 +1,29 @@
 from flask import render_template,session,redirect,url_for,current_app
 from app import db
-from app.models import User,Repository,Product
+from app.models import User,Repository,Product,Equipment
 from app.email import send_email
 from app.main import main
 
 @main.route('/workplace')
 def workplace():
-    return render_template('workplace.html')
+    # 返回设备列表
+    rpo_ept_list = []
+    transfer_ept_list = []
+    machine_ept_list = []
+    Equipments = Equipment.query.all()
+    for ept in Equipments:
+        item = {
+            'ept_name': ept.ept_name,
+            'status': ept.status,
+            'position': ept.position
+        }
+        if ept.position == '立体仓库':
+            rpo_ept_list.append(item)
+
+        elif ept.position == '传送物流':
+            transfer_ept_list.append(item)
+
+        elif ept.position == '自动机床':
+            machine_ept_list.append(item)
+    return render_template('workplace.html',
+                            rpo_ept_list = rpo_ept_list, transfer_ept_list = transfer_ept_list, machine_ept_list = machine_ept_list)
