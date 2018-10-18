fcd7fe922fa147085fdddfd60b5144fd5c827632
sk <xyk_sk@163.com>
2018-10-18 19:14:06 +0800

    数据库设备表建立

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,19 +6,24 @@ class NameForm(Form):
     name = StringField('请输入姓名?',validators=[DataRequired()])
     submit = SubmitField('submit')
 
+
 class Rpo_out(Form):
     submit = SubmitField('出库')
+
 
 class RepositoryForm(Form):
     row = IntegerField('行?',validators=[DataRequired()])
     column = IntegerField('列?', validators=[DataRequired()])
     status = RadioField(label='操作选择',choices=[('input_1', '入库(原料）'), ('input_2', '入库(成品）'),('output', '出库')],validators=[DataRequired()])
     submit = SubmitField('提交')
+
+
 class RepositoryInputForm(Form):
     row = IntegerField('行?', validators=[DataRequired()])
     column = IntegerField('列?', validators=[DataRequired()])
     product_id = IntegerField('产品编号',validators=[DataRequired()])
     submit = SubmitField('提交')
+
 
 class RepositoryOutputForm(Form):
     row = IntegerField('行?', validators=[DataRequired()])
@@ -27,18 +32,29 @@ class RepositoryOutputForm(Form):
 
 
 class ProductEditForm(Form):
-    id = IntegerField('产品编号?',validators=[DataRequired()])
-    order_id= IntegerField('订单号?',validators=[DataRequired()])
+    id = IntegerField('产品编号?', validators=[DataRequired()])
+    order_id= IntegerField('订单号?', validators=[DataRequired()])
     type = StringField('产品类型？', validators=[DataRequired()])
-    status = StringField('产品状态?',validators=[DataRequired()])
-    position = StringField('产品位置?',validators=[DataRequired()])
+    status = StringField('产品状态?', validators=[DataRequired()])
+    position = StringField('产品位置?', validators=[DataRequired()])
     submit = SubmitField('提交')
+
 
 class ProductAddForm(Form):
     id = IntegerField('产品编号?', validators=[DataRequired()])
     order_id = IntegerField('订单号?', validators=[DataRequired()])
-    type = StringField('产品类型？',validators=[DataRequired()])
+    type = StringField('产品类型？', validators=[DataRequired()])
     status = StringField('产品状态?', validators=[DataRequired()])
     position = StringField('产品位置?', validators=[DataRequired()])
     submit = SubmitField('提交')
+
+
+class EqupimentAddForm(Form):
+    eqt_name = IntegerField('设备名称', validators=[DataRequired()])
+    status = StringField('设备状态?', validators=[DataRequired()])
+    position = StringField('设备位置?', validators=[DataRequired()])
+    submit = SubmitField('提交')
+
+
+
 
--- app/models.py
+++ app/models.py
@@ -46,4 +46,8 @@ class Product(db.Model):
     def __repr__(self):
         return '<Product %r>' % self.id
 
-
+class Equpiment(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    ept_name = db.Column(db.String(64), unique=True, index=True)
+    status = db.Column(db.String(64))
+    position = db.Column(db.String(64))
